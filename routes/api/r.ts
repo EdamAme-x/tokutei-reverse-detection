a10229251c9f803c2a0b99c4326b2f2e76781c5b
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-11 11:13:07 +0900

    add

--- routes/api/r.ts
+++ routes/api/r.ts
@@ -1,10 +1,10 @@
-export async function handler(req: Request) {
+export async function handler(req: Request): Promise<Response> {
   const code = atob(new URL(req.url).searchParams.get("c") ?? "Error");
 
   const response = await fetch(
     `https://api.activetk.jp/urlmin/get?code=${code}`,
   );
-  const data: Record<string, string | number | Record<string, string>> =
+  const data: { status: string, LinkURL: string, CreatorInfo: { IPAddress: string, Location: string, TimeZone: string }, CreatedDateTimeAsUnixTime: number, UsedCount: number } =
     await response.json();
 
   if (data.status === "Error") {
@@ -17,57 +17,57 @@ export async function handler(req: Request) {
   }
 
   const parseContext = (
-    data: Record<string, string | number | Record<string, string>>,
+    data: { LinkURL: string, CreatorInfo: { IPAddress: string, Location: string, TimeZone: string }, CreatedDateTimeAsUnixTime: number, UsedCount: number },
   ) => {
     return {
       target: data.LinkURL,
       code: code,
       creator: {
-        ip: (data.CreatorInfo as Record<string, string>).IPAddress,
-        location: (data.CreatorInfo as Record<string, string>).Location,
-        country: (data.CreatorInfo as Record<string, string>).TimeZone,
+        ip: data.CreatorInfo.IPAddress,
+        location: data.CreatorInfo.Location,
+        country: data.CreatorInfo.TimeZone,
       },
       created_at: data.CreatedDateTimeAsUnixTime,
       uses_count: data.UsedCount,
     };
   };
 
-  const geo = await fetch("https://www.activetk.jp/tools/whois", {
+  const geo = await fetch("https://www.ipaddress.com/ip-lookup", {
     "headers": {
-      "accept": "*/*",
+      "accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       "accept-language": "ja,en-US;q=0.9,en;q=0.8",
-      "content-type":
-        "multipart/form-data; boundary=----WebKitFormBoundary5j3yGfB7uoDK4qPt",
-      "sec-ch-ua": '"Google Chrome";v="119", "Chromium";v="' +
-        Math.floor(Math.random() * 130) + '", "Not?A_Brand";v="24"',
+      "cache-control": "max-age=0",
+      "content-type": "application/x-www-form-urlencoded",
+      "sec-ch-ua":
+        '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
       "sec-ch-ua-mobile": "?0",
       "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
       "sec-fetch-site": "same-origin",
-      "x-requested-with": "XMLHttpRequest",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
     },
-    "referrer": "https://www.activetk.jp/tools/whois",
-    "referrerPolicy": "same-origin",
-    "body":
-      '------WebKitFormBoundary5j3yGfB7uoDK4qPt\r\nContent-Disposition: form-data; name="iana"\r\n\r\n' +
-      (data.CreatorInfo as Record<string, string>).HostName +
-      "\r\n------WebKitFormBoundary5j3yGfB7uoDK4qPt--\r\n",
+    "referrer": "https://www.ipaddress.com/ip-lookup",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "host=" + data.CreatorInfo.IPAddress,
     "method": "POST",
     "mode": "cors",
     "credentials": "include",
   });
 
+  const html = await geo.text();
+  const latitude = html.split("Latitude</th><td>")[1].split(" /")[0];
+  const longitude = html.split("Longitude</th><td>")[1].split(" /")[0];
+
   return new Response(JSON.stringify({
     result: "OK",
     ...parseContext(data),
-    whois: (await geo.text()).split("\n").map((line) => {
-      const [key, value] = line.split(":");
-      return {
-        key: (key ?? "").trim(),
-        value: (value ?? "").trim(),
-      };
-    }),
+    point: {
+      latitude: latitude,
+      longitude: longitude,
+    }
   }));
 }
 
